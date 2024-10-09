994bf97d2ae08633050b4fc3ba6f81463676849d
leonleerl <leon.runlongli@gmail.com>
2024-10-09 19:19:51 +0800

    add Detail Navigation

---
+++ BethanysPieShopHRM/Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,19 @@
+using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components.Pages;
+
+public partial class EmployeeDetail : ComponentBase
+{
+    
+    [Parameter]
+    public int EmployeeId { get; set; }
+
+    public Employee Employee { get; set; } = new();
+
+    protected override void OnInitialized()
+    {
+        Employee = MockDataService.Employees.Single(e => e.EmployeeId == EmployeeId);
+    }
+}
