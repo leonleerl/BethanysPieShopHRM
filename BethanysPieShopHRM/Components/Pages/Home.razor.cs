ed32daa7cb29884e13f6b13a658ae7051da5da19
leonleerl <leon.runlongli@gmail.com>
2024-10-21 10:43:43 +0800

    load Widgets

---
+++ BethanysPieShopHRM/Components/Pages/Home.razor.cs
@@ -0,0 +1,11 @@
+using BethanysPieShopHRM.Components.Widgets;
+
+namespace BethanysPieShopHRM.Components.Pages;
+
+public partial class Home
+{
+    public List<Type> Widgets { get; set; } = new()
+    {
+        typeof(EmployeeCountWidget), typeof(InboxWidget)
+    };
+}
