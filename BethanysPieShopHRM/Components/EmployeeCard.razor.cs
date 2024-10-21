9a09e5fa34471fe5258ae67955859c1b4c7252f8
leonleerl <leon.runlongli@gmail.com>
2024-10-21 10:52:57 +0800

    error boundaries

--- BethanysPieShopHRM/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM/Components/EmployeeCard.razor.cs
@@ -10,4 +10,12 @@ public partial class EmployeeCard
     public Employee Employee { get; set; } = default!;
     [Parameter]
     public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (string.IsNullOrEmpty(Employee.LastName))
+        {
+            throw new Exception("Last name can't be empty");
+        }
+    }
 }
